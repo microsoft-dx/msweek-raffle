5e85f4d286d862575b0d543286001e1bbe3489dd
Laurentiu Stamate <laurentiu.stamate@equilobe.com>
2018-03-02 19:59:44 +0100

    Add SQL script for table

---
+++ MSWeekRaffle/Database/dbo.Registrations.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Registrations] (
+    [Id]             INT            IDENTITY (1, 1) NOT NULL,
+    [EmailAddress]   NVARCHAR (200) NOT NULL,
+    [ValidationCode] NVARCHAR (200) NOT NULL,
+    [IsValidated]    BIT            NULL,
+    [CreatedDate]    DATETIME2 (7)  NOT NULL,
+    [ActivatedDate]  DATETIME2 (7)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
